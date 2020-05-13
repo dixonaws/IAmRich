8ab22fa394121b805ceb11275b3f6e05e84f1701
John Dixon <dixonaws@amazon.com>
2020-05-13 21:42:50 +0200

    added readme

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by John Dixon on 5/13/20.
+//  Copyright © 2020 John Dixon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# IAmRich
+This is the sample app discussed in a iOS 13 & Swift 5 Udemy course
+
